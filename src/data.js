45ea18d78130dceb372a5711e4f310dde7943a0c
Adewale Oluwatoyin <adewaleoluwatoyin81@gmail.com>
2023-05-29 03:07:11 +0100

    A Simple Illustration of Search using Filter Method

---
+++ src/data.js
@@ -0,0 +1,62 @@
+import img1 from './images/img1.jpg'
+import img2 from './images/img2.jpg'
+import img3 from './images/img3.jpg'
+import img4 from './images/img4.jpg'
+import img5 from './images/img5.jpg'
+import img6 from './images/img6.jpg'
+import img7 from './images/img7.jpg'
+import img8 from './images/img8.jpg'
+
+const data = {
+cardData : [
+    {
+        id:1,
+        img: img1,
+        title: 'Blog 1',
+        desc: 'Description 1'
+    },
+    {
+        id:2,
+        img: img2,
+        title: 'Blog 2',
+        desc: 'Description 2'
+    },
+    {
+        id:3,
+        img: img3,
+        title: 'Blog 3',
+        desc: 'Description 3'
+    },
+    {
+        id:4,
+        img: img4,
+        title: 'Blog 4',
+        desc: 'Description 4'
+    },
+    {
+        id:5,
+        img: img5,
+        title: 'Blog 5',
+        desc: 'Description 5'
+    },
+    {
+        id:6,
+        img: img6,
+        title: 'Blog 6',
+        desc: 'Description 6'
+    },
+    {
+        id:7,
+        img: img7,
+        title: 'Blog 7',
+        desc: 'Description 7'
+    },
+    {
+        id:8,
+        img: img8,
+        title: 'Blog 8',
+        desc: 'Description 8'
+    },   
+]
+}
+export default data;
